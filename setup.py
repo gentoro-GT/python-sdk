5405fdcdd5e573af3bec20e18d0e37b43ec758c9
Mohit_tripathi <mohit.tripathi@teqfocus.com>
2025-02-17 18:25:06 +0530

    adding License file along with Requirments

--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+
+def read_requirements():
+    with open("requirements.txt", encoding="utf-8") as req_file:
+        return req_file.read().splitlines()
+
 
 setuptools.setup(
     name="Gentoro",
     version="0.1.0",
     author="Gentoro",
     author_email="",
-    description="A Python library ",
-    long_description=long_description,
+    description="Gentoro Python SDK for AI tool execution and authentication",
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/     /Gentoro",  # Need Repo URL here
+    url="https://github.com/gentoro-GT/python-sdk",  # Need Repo URL here
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
+    install_requires=read_requirements(),
+    include_package_data=True,
+    license="Apache-2.0",
 )
-
